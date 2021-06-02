bf821877ca0e76e24a0c6317feeaf6b54b12b52b
kmsko <63438051+kmsko@users.noreply.github.com>
2021-06-02 20:05:32 +0300

    add logi and password

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,15 +2,18 @@ import { Field, Form, Formik } from 'formik';
 import React from 'react';
 import { connect } from 'react-redux';
 import s from './login.module.css';
-import {login} from '../../redux/auth-reducer'
+import { login } from '../../redux/auth-reducer'
 import { Redirect } from 'react-router';
 let Login = (props) => {
 
-    if(props.id){
+    if (props.id) {
         return <Redirect to={`/profile/${props.id}`} />
     }
     return (
         <div >
+         
+            <div>Логин: kmsko@ya.ru</div>
+            <div>Password: 1234 </div>
             <h2 className={s.titlePageLogin}>Вход</h2>
             <Formik
                 initialValues={{ email: '', password: '', rememberMe: false }}
@@ -24,23 +27,25 @@ let Login = (props) => {
                         <div className={s.textEmail}>Email</div>
                         <Field type="email" name="email" placeholder="email@" className={s.emailInput} />
                         <div className={s.textPass}>Password</div>
-                        <Field  type="password" name="password" placeholder="password" autoComplete="current-password" className={s.passwordInput} />
+                        <Field type="password" name="password" placeholder="password" autoComplete="current-password" className={s.passwordInput} />
                         <div >Запоимнить меня</div>
                         <Field type="checkbox" name="rememberMe" />
                         <div>
                             <button className={s.buttonLogin} type="submit">
                                 Войти
                              </button>
+
                         </div>
                     </Form>
                 }
             </Formik>
+
         </div>
     )
 }
 const mapStateToProps = (state) => {
     return {
-      id: state.auth.id,
+        id: state.auth.id,
     }
-  }
-export default connect( mapStateToProps , {login})(Login) 
+}
+export default connect(mapStateToProps, { login })(Login)
